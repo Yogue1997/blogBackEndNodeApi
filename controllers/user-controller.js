03066ba217d47882ca78828e7d43ed34928171cd
Yogue1997 <yousyogue@gmail.com>
2023-12-26 00:58:43 -0500

    Login to return data to use in frontend

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -72,7 +72,7 @@ const login = async(req, res, next) => {
   if(!isPasswordCorrect){
     return res.status(400).json({ message: 'Incorrect Password' });
   }
-  return res.status(200).json({ message: 'Login Successfull' })
+  return res.status(200).json({existingUser})
 }
 
 module.exports = { getAllUsers, signup, login };
